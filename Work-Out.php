c74f202acfdbf9a1c30a24d85b9dfda38dd7e84f
bonbo0811 <abcdef999gp811@gmail.com>
2022-05-29 17:45:39 +0900

    Work-Outイメージ変更、説明変更

--- Work-Out.php
+++ Work-Out.php
@@ -22,7 +22,8 @@
                     </li>
                     <li>
                         <h3>Work-Out</h3>
-                        <span class ="workpage color-blue">HTML / CSS /PHP / Laravel / MySQL</span>
+                        <span class ="workpage color-blue">HTML / CSS /PHP / Laravel / MySQL</span><br>
+                        <span class ="workpage color-blue">AWS / Lightsail </span>
                         <div class="link justify-content-left workpage">
                             <a class="btn btn-outline-secondary" href="http://13.115.77.9/" target="_blank" rel="noopener noreferrer">Webサイト</a>
                             <a class="btn btn-outline-secondary" href="https://github.com/bonbo0811/Work-Out" target="_blank" rel="noopener noreferrer">GitHub</a>
@@ -30,7 +31,6 @@
                         <p class="workpage">
                             ログインユーザーごとにプロジェクト単位のToDoを管理できるWebアプリケーションです。<br><br>
                             チームでの作業にも対応するため、プロジェクトやToDoに担当メンバーを割り当てる機能が付いています。<br><br>
-                            現在はまだ未公開です。<br><br>
                             
                         </p>                  
                     </li>
